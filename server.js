b45e558e9ede94988d326ac7e4ece199a515ac83
Sean Driscoll <spdriscoll@umass.edu>
2022-11-17 10:33:50 -0500

    testing get

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.static('.'))
 app.use(express.static('images'))
 
 app.get('/', (req, res) => {
-        res.sendFile('index.html', { root: '.' })
+        //res.sendFile('index.html', { root: '.' })
+        res.send("working");
     });
   
 app.listen(port, () => {
