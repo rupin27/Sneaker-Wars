a4b7778b9986b2427d71cb915cbdb8eba11baf6b
Sean Driscoll <spdriscoll@umass.edu>
2022-11-16 21:01:44 -0500

    added procfile and testing file serving

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 'use strict';
 import express from 'express';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-        res.send('Home');
+    console.log(__dirname + '\\index.html');
+    res.sendFile(__dirname + '\\index.html');
     });
   
 app.listen(port, () => {
