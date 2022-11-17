c9a62e6587072a42dd1786df76b1dd66373dbb9a
Sean Driscoll <spdriscoll@umass.edu>
2022-11-17 10:52:08 -0500

    added dynamic port

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ import express from 'express';
 import fs from 'fs';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.use(express.static('public'))
 app.use(express.static('.'))
 app.use(express.static('images'))
 
 app.get('/', (req, res) => {
         //res.sendFile('index.html', { root: '.' })
+        console.log('get index.html request received');
         res.send("working");
     });
   
