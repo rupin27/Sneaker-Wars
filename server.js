172a7d266d63b6058d1696b5ee327bc4e42f31f8
Sean Driscoll <spdriscoll@umass.edu>
2022-11-17 22:29:52 -0500

    testing new server file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 /**
  * 'use strict';
 import express from 'express';
@@ -8,26 +7,17 @@ const port = process.env.PORT || 8080;
 app.use(express.static('public'))
 app.use(express.static('.'))
 app.use(express.static('images'))
-
-app.get('/', (req, res) => {
-        //res.sendFile('index.html', { root: '.' })
-        console.log('get index.html request received');
-        res.send("working");
-    });
-    
-  
-app.listen(port, () => {
-        console.log('Listening on port ', port);
-    });
-
+*/
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 8080
-**/
-const express = require('express')
-const path = require('path')
+const app = express();
 const PORT = process.env.PORT || 5000
-express()
-  .use('/', express.static('./html'))
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use('/', express.static('./html'));
+
+app.get('/', (req, res) => {  //send index.html at root
+    res.sendFile("index");
+  });
+
+app.listen(process.env.PORT || 5000, function(){//listen at process' port
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
