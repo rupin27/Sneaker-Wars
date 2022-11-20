fb64af699d09e8a6795ecdfbf89f44b494c8d63c
Sean Driscoll <spdriscoll@umass.edu>
2022-11-20 14:33:19 -0500

    added search bar functioniality

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 import express from 'express';
 import SneaksAPI from 'sneaks-api';
 const app = express();
+const router = express.Router();
 const sneaks = new SneaksAPI();
 const PORT = process.env.PORT || 5000
 app.use('/', express.static('./html'));
+
 
 app.get('/', (req, res) => {  //send index.html at root
   res.sendFile("index");
   });
 
-  app.get('/search', (req, res) => {  //send index.html at root
+app.get('/search', (req, res) => { 
     //getProducts(keyword, limit, callback) takes in a keyword and limit and returns a product array 
-    sneaks.getProducts("chuck", 1, function(err, products){
+    sneaks.getProducts(req.query.shoeName, 1, function(err, products){
     res.send(products[0]);
-    console.log('hi hi');
+  });
     
-    });
-    
-    });
+});
+
+//add searchN to search more than 1
+
 
 app.listen(PORT || 5000, function(){//listen at process' port
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
