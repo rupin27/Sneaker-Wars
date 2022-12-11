968e887f8aa83490ce08c78f88b09faeffe55847
Rupin Mehra <rupinmehra@umass.edu>
2022-12-11 18:36:09 -0500

    importing crud functionality

--- html/login.js
+++ html/login.js
@@ -1,3 +1,5 @@
+import * as crud from '../database.js';
+
 document.getElementById("sign_in").addEventListener('click', () =>{
     window.localStorage.setItem("username", document.getElementById("username_input").value);
     window.localStorage.setItem("password", document.getElementById("password_input").value);
