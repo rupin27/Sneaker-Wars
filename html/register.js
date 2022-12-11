1f81ec9271855f4c71ba0fc94bd3dc0c9d62f6d4
Rupin Mehra <rupinmehra@umass.edu>
2022-12-11 18:43:16 -0500

    save information for register

--- html/register.js
+++ html/register.js
@@ -1,3 +1,4 @@
+import * as crud from '../database.js';
 document.getElementById("register").addEventListener('click', () => {
     try {
         window.localStorage.setItem("username_for_reg", document.getElementById("username_input_reg").value);
@@ -9,5 +10,8 @@ document.getElementById("register").addEventListener('click', () => {
     }
 });
 
-
+document.getElementById("register").addEventListener('click', () => {
+    crud.createAccount(document.getElementById("username_input_reg").value, document.getElementById("password_input_reg").value, "none", "none", "none", "none", 0, 0, "none", "none", "none");
+ //                                    userName                                       userPass                                userImg userLocation about pairs followers, following, favorites, owned, want
+ });
 
