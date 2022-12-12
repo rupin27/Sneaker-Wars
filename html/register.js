b6ca24623ef0fe69926fa14ae34b93e20105a6fa
Rupin Mehra <rupinmehra@umass.edu>
2022-12-11 20:51:23 -0500

    register functionality

--- html/register.js
+++ html/register.js
@@ -1,4 +1,3 @@
-import * as crud from '../database.js';
 document.getElementById("register").addEventListener('click', () => {
     try {
         window.localStorage.setItem("username_for_reg", document.getElementById("username_input_reg").value);
@@ -6,12 +5,26 @@ document.getElementById("register").addEventListener('click', () => {
         alert("You have been registered successfully! Redirecting to the login page");
     }
     catch(err) {
-        adddlert("Please try again");
+        alert("Please try again");
     }
 });
 
-document.getElementById("register").addEventListener('click', () => {
-    crud.createAccount(document.getElementById("username_input_reg").value, document.getElementById("password_input_reg").value, "none", "none", "none", "none", 0, 0, "none", "none", "none");
- //                                    userName                                       userPass                                userImg userLocation about pairs followers, following, favorites, owned, want
- });
+// document.getElementById("register").addEventListener('click', () => {
+//     crud.createAccount(document.getElementById("username_input_reg").value, document.getElementById("password_input_reg").value, "none", "none", "none", "none", 0, 0, "none", "none", "none");
+//  //                                    userName                                       userPass                                userImg userLocation about pairs followers, following, favorites, owned, want
+//  });
 
+ (async function render(){
+    console.log('fetching user from DB');
+    const response = await fetch('/createAccount?userName=' + document.getElementById("username_input_reg").value + '&userPass=' + document.getElementById("password_input_reg").value + '&userImg=' + "none" + '&userLocation=' + "none" + '&about=' + "none" + '&pairs=' + "none" + '&followers=' + 0 + '&following=' + 0 + '&favorites=' + "none" + '&owned=' + "none" + '&want=' + "none");
+    if (response.ok) {
+        console.log("response ok");
+        let userJSON = await response.json();
+        console.log(userJSON)
+        alert("You have been registered successfully! Redirecting to the login page");
+    }
+    else{
+        alert('error fetching user');
+    }
+
+})();
