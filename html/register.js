8e4d0696f8cd3ba58f675352f322f5bf61bdbe60
Rupin Mehra <rupinmehra@umass.edu>
2022-12-11 22:12:27 -0500

    samp

--- html/register.js
+++ html/register.js
@@ -92,7 +92,7 @@
 const response = await fetch('/createAccount', {
 method: 'POST',
 headers: {
-  'Content-Type': 'application/json;charset=utf-8' },
+  'Content-Type': 'application/json;charset=utf-8' 
 },
 body: `{
    userName: document.getElementById("username_input_reg").value, 
