a26d2800110f644a1ddfe452f724b7b505999088
Rupin Mehra <rupinmehra@umass.edu>
2022-12-11 21:03:29 -0500

    edits

--- html/register.js
+++ html/register.js
@@ -5,26 +5,25 @@
 
 document.getElementById("register").addEventListener('click', async () => {
     console.log('fetching user from DB');
-    const response = await fetch('/createAccount?userName=' + document.getElementById("username_input_reg").value + '&userPass=' + document.getElementById("password_input_reg").value + '&userImg=' + "none" + '&userLocation=' + "none" + '&about=' + "none" + '&pairs=' + "none" + '&followers=' + 0 + '&following=' + 0 + '&favorites=' + "none" + '&owned=' + "none" + '&want=' + "none");
+    const response = await fetch('/createAccount?userName=' + document.getElementById("username_input_reg").value + '&userPass=' + document.getElementById("password_input_reg").value + '&userImg=' + "none" + '&userLocation=' + "none" + '&about=' + "none" + '&pairs=' + 0 + '&followers=' + 0 + '&following=' + 0 + '&favorites=' + "none" + '&owned=' + "none" + '&want=' + "none");
     if (response.ok) {
         console.log("response ok");
         let userJSON = await response.json();
         console.log(userJSON)
         alert("You have been registered successfully! Redirecting to the login page");
     }
-    else{
+    else {
         alert('error fetching user');
     }
-
 });
 
-document.getElementById("register").addEventListener('click', () => {
-    try {
-        window.localStorage.setItem("username_for_reg", document.getElementById("username_input_reg").value);
-        window.localStorage.setItem("password_for_reg", document.getElementById("password_input_reg").value);
-        alert("You have been registered successfully! Redirecting to the login page");
-    }
-    catch(err) {
-        alert("Please try again");
-    }
-});
+// document.getElementById("register").addEventListener('click', () => {
+//     try {
+//         window.localStorage.setItem("username_for_reg", document.getElementById("username_input_reg").value);
+//         window.localStorage.setItem("password_for_reg", document.getElementById("password_input_reg").value);
+//         alert("You have been registered successfully! Redirecting to the login page");
+//     }
+//     catch(err) {
+//         alert("Please try again");
+//     }
+// });
