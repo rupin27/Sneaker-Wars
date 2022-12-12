5ce0ea73cafeb16afde2b60183202b9abde7f147
Rupin Mehra <rupinmehra@umass.edu>
2022-12-11 23:52:27 -0500

    back to og edits

--- html/login.js
+++ html/login.js
@@ -1,24 +1,28 @@
-import MiniCrypt from '../server/miniCrypt.js'
+// import MiniCrypt from '../server/miniCrypt.js'
 
-let users = {}
-const mc = new MiniCrypt();
+// let users = {}
+// const mc = new MiniCrypt();
 
-let userName = document.getElementById("username_input").value
-let userPass = document.getElementById("password_input").value
+// let userName = document.getElementById("username_input").value
+// let userPass = document.getElementById("password_input").value
 
+// document.getElementById("sign_in").addEventListener('click', () => {
+//     let users = JSON.parse(window.localStorage.getItem("users"));
+//     let currUser = '';
+//     for (let i = 0; i < users.length; i++) {
+//         if (users[i][0] == userName) {
+//             currUser = users[i];
+//         }
+//         else {
+//             alert("Create an account first!")
+//         }
+//     }
+//     if (mc.check(userPass, currUser[1], currUser[2]) == true) {
+//         window.localStorage.setItem("username", userName);
+//         window.localStorage.setItem("password", userPass);
+//     }
+// });
 document.getElementById("sign_in").addEventListener('click', () => {
-    let users = JSON.parse(window.localStorage.getItem("users"));
-    let currUser = '';
-    for (let i = 0; i < users.length; i++) {
-        if (users[i][0] == userName) {
-            currUser = users[i];
-        }
-        else {
-            alert("Create an account first!")
-        }
-    }
-    if (mc.check(userPass, currUser[1], currUser[2]) == true) {
-        window.localStorage.setItem("username", userName);
-        window.localStorage.setItem("password", userPass);
-    }
+    window.localStorage.setItem("username", document.getElementById("username_input").value);
+    window.localStorage.setItem("password", document.getElementById("password_input").value);
 });
--- html/register.js
+++ html/register.js
@@ -1,4 +1,4 @@
-import MiniCrypt from '../server/miniCrypt.js'
+// import MiniCrypt from '../server/miniCrypt.js'
 // userName VARCHAR UNIQUE NOT NULL, 
 // userPass VARCHAR NOT NULL,
 // userImg  VARCHAR,
@@ -11,24 +11,38 @@ import MiniCrypt from '../server/miniCrypt.js'
 // owned VARCHAR,
 // want VARCHAR
 
-let users = {}
-const mc = new MiniCrypt();
-let userName = document.getElementById("username_input_reg").value
-let userPass = document.getElementById("password_input_reg").value
+// let users = {}
+// const mc = new MiniCrypt();
+// let userName = document.getElementById("username_input_reg").value
+// let userPass = document.getElementById("password_input_reg").value
+
+// document.getElementById("register").addEventListener('click', () => {
+//     (async function registerUser() {
+//         console.log('fetching user from DB');
+//         const response = await fetch('/createAccount?userName=' + userName + '&userPass=' + userPass + '&userImg=' + "none" + '&userLocation=' + "none" + '&about=' + "none" + '&pairs=' + 0 + '&followers=' + 0 + '&following=' + 0 + '&favorites=' + "none" + '&owned=' + "none" + '&want=' + "none", {method: 'POST'});
+//         if(response.ok){
+//             let userJSON = await response.json(); 
+//             let uName = userName; //userJSON.rows[0].username
+//             let uPass = userPass; //userJSON.rows[0].userpass
+//             let encryptedPassword = mc.hash(uPass);
+//             let salt = encryptedPassword[0];
+//             let hash = encryptedPassword[1];
+//             users.push([uName, salt, hash]);
+//             window.localStorage.setItem("users", JSON.stringify(users));
+//             alert("You have been registered successfully! Redirecting to the login page");
+//             }
+//             else{
+//                 alert('error fetching user');
+//             }
+//     })();
+// });
 
 document.getElementById("register").addEventListener('click', () => {
     (async function registerUser() {
         console.log('fetching user from DB');
-        const response = await fetch('/createAccount?userName=' + userName + '&userPass=' + userPass + '&userImg=' + "none" + '&userLocation=' + "none" + '&about=' + "none" + '&pairs=' + 0 + '&followers=' + 0 + '&following=' + 0 + '&favorites=' + "none" + '&owned=' + "none" + '&want=' + "none", {method: 'POST'});
+        const response = await fetch('/createAccount?userName=' + document.getElementById("username_input_reg").value + '&userPass=' + document.getElementById("password_input_reg").value + '&userImg=' + "none" + '&userLocation=' + "none" + '&about=' + "none" + '&pairs=' + 0 + '&followers=' + 0 + '&following=' + 0 + '&favorites=' + "none" + '&owned=' + "none" + '&want=' + "none", {method: 'POST'});
         if(response.ok){
             let userJSON = await response.json(); 
-            let uName = userName; //userJSON.rows[0].username
-            let uPass = userPass; //userJSON.rows[0].userpass
-            let encryptedPassword = mc.hash(uPass);
-            let salt = encryptedPassword[0];
-            let hash = encryptedPassword[1];
-            users.push([uName, salt, hash]);
-            window.localStorage.setItem("users", JSON.stringify(users));
             alert("You have been registered successfully! Redirecting to the login page");
             }
             else{
@@ -36,5 +50,3 @@ document.getElementById("register").addEventListener('click', () => {
             }
     })();
 });
-
-
