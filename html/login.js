06637c074438fe514e455c8dfb8902dbf432182a
Rupin Mehra <rupinmehra@umass.edu>
2022-12-11 23:41:31 -0500

    encryption functionality

--- html/login.js
+++ html/login.js
@@ -1,10 +1,24 @@
-document.getElementById("sign_in").addEventListener('click', () => {
-    window.localStorage.setItem("username", document.getElementById("username_input").value);
-    window.localStorage.setItem("password", document.getElementById("password_input").value);
-});
+import {MiniCrypt} from '../server/miniCrypt.js'
 
-document.getElementById("register").addEventListener('click', () => {
-    
-    window.localStorage.setItem("username", document.getElementById("username_input").value);
-    window.localStorage.setItem("password", document.getElementById("password_input").value);
+let users = {}
+const mc = new MiniCrypt();
+
+let userName = document.getElementById("username_input").value
+let userPass = document.getElementById("password_input").value
+
+document.getElementById("sign_in").addEventListener('click', () => {
+    let users = JSON.parse(window.localStorage.getItem("users"));
+    let currUser = '';
+    for (let i = 0; i < users.length; i++) {
+        if (users[i][0] == userName) {
+            currUser = users[i];
+        }
+        else {
+            alert("Create an account first!")
+        }
+    }
+    if (mc.check(userPass, currUser[1], currUser[2]) == true) {
+        window.localStorage.setItem("username", userName);
+        window.localStorage.setItem("password", userPass);
+    }
 });
